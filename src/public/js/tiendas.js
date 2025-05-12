60922111c7fd27ba38c02b233f1b2028d62ecce8
Bram-Stoker <l20211817@tectijuana.edu.mx>
2025-05-12 13:48:12 -0700

    Version 3.0.0

--- src/public/js/tiendas.js
+++ src/public/js/tiendas.js
@@ -5,9 +5,22 @@ document.addEventListener('DOMContentLoaded', async () => {
             headers: { "Content-Type": "application/json" },
         });
 
-        const result = await res.json();
+        result.body.forEach((activo) => {
+            const renglon = document.createElement("tr");
+            renglon.dataset.id = activo.id_activofijo;
+    
+            var contenedor = document.getElementById("multiple_stores");
+            
+            renglon.innerHTML = `
+                <div class="store-name">
+                    <h5>${activo.nombre_tienda}</h5>
+                </div>
+            `;
+            
+            contenedor.appendChild(renglon);
+        });
 
-        console.log(result)
+        console.log()
   
     } catch (err) {
       console.error("Error en la carga de resultados:", err.message);
